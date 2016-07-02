4acbba382d721c650abda0e8294284227c49aef2
Samuel Kitazume <samuel.kitazume@gmail.com>
2016-07-02 02:46:18 -0300

    Criação das interações de persistência e alterações no model da wallet

--- src/app/components/wallet/wallet.service.js
+++ src/app/components/wallet/wallet.service.js
@@ -3,13 +3,13 @@
 
   angular
     .module('s2it')
-    .factory('walletService', ["localstorageService", walletService]);
+    .factory('walletService', ["localstorageService", "$rootScope", walletService]);
 
-  function walletService(lsService) {
+  function walletService(lsService, $rootScope) {
     var wallet = {},
         op = lsService.getItem("operations");
 
-    wallet.total = lsService.getItem("total") || 0;
+    wallet.total = +lsService.getItem("total") || 0;
     wallet.operations = op ? JSON.parse(op) : [];
 
     return {
@@ -23,11 +23,21 @@
                 return wallet.operations.filter(function(row) { return row.type == "out" })
             } else return wallet.operations
         },
-        setOperation: function setOperation() {
-
+        operate: function operate(val) {
+            var positive = val>=0;
+            wallet.total += val;
+            wallet.operations.push({
+                type: positive ? "in" : "out",
+                value: val
+            });
+            $rootScope.$broadcast("wallet.changed");
+        },
+        persist: function persist() {
+            lsService.setItem("total", wallet.total);
+            lsService.setItem("operations", JSON.stringify(wallet.operations));
         }
     };
     
-  }; 
+  }
 
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,14 +3,32 @@
 
   angular
     .module('s2it')
-    .controller('MainController', ["walletService", "localstorageService", MainController]);
+    .controller('MainController', ["walletService", "localstorageService", "$rootScope", MainController]);
 
   /** @ngInject */
-  function MainController(walletService, lsService) {
+  function MainController(walletService, lsService, $rootScope) {
     var vm = this;
 
-    vm.balance = walletService.getTotal();
-    vm.operations = walletService.getOperations();
+    vm.operationFieldValue = 0;
+
+    var update = function update() {
+      vm.balance = walletService.getTotal();
+      vm.operations = walletService.getOperations();      
+    };
+
+    update();
+
+    vm.save = function save() {
+      if (vm.operationFieldValue != undefined) {
+        walletService.operate(+vm.operationFieldValue);
+      }
+    };
+
+    vm.persist = function persist() {
+      walletService.persist();
+    };
+
+    var walletChangedEvent = $rootScope.$on("wallet.changed", update);
     
   }
 })();
